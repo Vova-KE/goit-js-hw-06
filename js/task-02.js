f149cfc36a58e850ea41dbe411a3e4321a11b7c6
Volodymyr Kozyr <vroger4@gmail.com>
2022-04-28 23:39:08 +0300

    homework#6
    second task done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createListItemWithMarkup = ingredients.map(ingredient => {
+  const createListItem = document.createElement('li');
+  createListItem.classList.add('item');
+  createListItem.textContent = ingredient;
+
+  return createListItem;
+})
+
+const list = document.getElementById('ingredients');
+
+list.append(...createListItemWithMarkup);
+
