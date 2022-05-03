a2a1496465a29add0ac696ade7b5dba06c24fdd9
Volodymyr Kozyr <vroger4@gmail.com>
2022-05-03 22:40:09 +0300

    homework#6
    task 6 update

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,17 @@ const validationInput = document.querySelector('#validation-input');
 validationInput.addEventListener("blur", (event) => {
     validationInput.textContent = event.currentTarget.value;
 
-    if (validationInput.textContent.length !== Number(validationInput.getAttribute('data-length'))) {
-        validationInput.classList.remove('valid');
-        validationInput.classList.add('invalid');
+    if (validationInput.textContent.length === Number(validationInput.getAttribute('data-length'))) {
+        validationInput.classList.add('valid');
+        if (validationInput.classList.contains('invalid')) {
+            validationInput.classList.remove('invalid');
+        }
+    } else if (validationInput.textContent.length === 0) {
+        validationInput.classList.remove('valid','invalid');
     } else {
-        validationInput.classList.replace('invalid','valid');
-    }
+        validationInput.classList.add('invalid');
+        if (validationInput.classList.contains('valid')) {
+            validationInput.classList.remove('valid');
+        }
+    } 
 });
